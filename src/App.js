61907dd65c4cda1e45c0cce60d564947ebcd3ad5
switchalves7 <gabriel.alves.amorim@hotmail.com>
2021-09-14 18:01:03 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import react from 'react'
+import react, { Component } from 'react'
 import  { robots } from './robots';
 import CardsList from './components/CardsList';
 import Searchbox from './Searchbox';
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1>RoboFriends</h1>
-            <Searchbox />
-            <CardsList robots={robots}/>
-        </div>
-    );
-};
+const state = {
+    robots: robots,
+    searchfield: ''
 
+}
+
+class App extends { Component }{
+    render () {
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <Searchbox />
+                <CardsList robots={robots}/>
+            </div>
+        );
+    }
+}
 
 
 
