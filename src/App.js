91e97c91c6c0ef39119d7ca1a544b27712b880bf
switchalves7 <gabriel.alves.amorim@hotmail.com>
2021-09-15 17:36:58 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import react, { Component } from 'react'
-import  { robots } from './robots';
 import CardsList from './components/CardsList';
 import Searchbox from './Searchbox';
 
@@ -10,10 +9,17 @@ class App extends  Component {
     constructor(){
         super()
         this.state = {
-                robots: robots,
+                robots: [],
                 searchfield: ''      
         }
     }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => this.setState({ robots: users}))
+    }
+
 
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value})
